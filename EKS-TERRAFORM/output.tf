8e34d0fb5044a4b015e634c3040d309de01f1dce
Gokul-Prabakran1 <165556566+Gokul-Prabakran1@users.noreply.github.com>
2024-12-07 23:27:54 +0530

    Update output.tf

--- EKS-TERRAFORM/output.tf
+++ EKS-TERRAFORM/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.gokul.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.gokul.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.gokul_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.gokul_subnet[*].id
 }
